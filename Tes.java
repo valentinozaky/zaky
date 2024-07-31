6c7d9d3001a1f240323f1eb4868a54987ef6c75c
valentinozaky <ananda_zaky_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:15:55 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[]args){
+        String nama = "Zaky";
+        String kelas = "X RPL 5";
+        System.out.print("halo saya" + nama + "dari kelas " + kelas);
+    }
+}
